9957d84da08c6627f7710c8b975e7ac11290a173
DuresameenShafi <duresameen32@gmail.com>
2024-09-30 13:17:28 -0700

    Login_Signin_interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./pages/register";
-import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Home from  "./pages/Home";
 import Map from "./pages/Map"
 function App() {
@@ -8,9 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Register />} />
        <Route path="/Home" element={<Home/>}/>
-        <Route path="/register" element={<Signup />} />
-        <Route path="/Login" element={<Login />} />
         <Route path="/Map" element={<Map/>}/>
       </Routes>
     </BrowserRouter>
